ddf9d2dbd2e23cc008affb438bae1d4557d0a805
varungupta04 <87354396+varungupta04@users.noreply.github.com>
2023-05-14 13:29:47 +0530

    Update projects.js

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -10,20 +10,20 @@ const projects = [
     id: uuidv4(),
     name: 'Doctor-appointment',
     desc:
-      'MERN prject that helps users get an appointment from doctors and communicate wuth them in this online phase live link in the repo (switch to master branch)https://github.com/varungupta04/doc-proj ',
+      '(switch to master branch)https://github.com/varungupta04/doc-proj ',
     img: UTrackerImg,
   },
   {
     id: uuidv4(),
     name: 'edu4u',
     desc:
-      'on going project that helps tution centres freelance websites for their communication and assignments https://github.com/varungupta04/backend-edu4u',
+      'https://github.com/varungupta04/backend-edu4u',
     img: GreenCtgImg,
   },
   {
     id: uuidv4(),
     name: 'landing page',
-    desc: 'using react to make landing pages with animations and smooth scroll complete front end https://github.com/varungupta04/Gifting_solution   https://github.com/varungupta04/3d-wesite',
+    desc: 'https://github.com/varungupta04/Gifting_solution   https://github.com/varungupta04/3d-wesite',
     img: CoinTrackerImg,
   },
   {
